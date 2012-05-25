117194868749c6f14fd62b1e5702a1c741d0a200
Chad Waters <authorblues@gmail.com>
2012-05-25 13:38:16 -0400

    Better setup script (handles non-alnum characters correctly)

--- setup-bot.sh
+++ setup-bot.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-botname=$1
+botname=$*
+botfile=`echo $botname | sed "s/[^a-zA-Z0-9]//g"`
+botdir=bot-$botfile
+
 cd `dirname $0`
 rootdir=`pwd`
 
-mkdir "bot-$botname"
+mkdir $botdir
 
-sed "s/BOTNAME/$botname/g" example-bots/Makefile.template > bot-$botname/Makefile
-cp example-bots/bcb-client.c bot-$botname/
-cp example-bots/bcb-client.h bot-$botname/
-sed "s/BOTNAME/$botname/g" example-bots/bot-template.c > bot-$botname/$botname.c
+sed "s/BOTNAME/$botfile/g" example-bots/Makefile.template > $botdir/Makefile
+cp example-bots/bcb-client.c $botdir/
+cp example-bots/bcb-client.h $botdir/
+sed "s/BOTNAME/$botname/g" example-bots/bot-template.c > $botdir/$botfile.c
