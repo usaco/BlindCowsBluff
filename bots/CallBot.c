0b12812b693bd5b29401475633e33a68d6c332f5
Chad Waters <authorblues@gmail.com>
2012-05-29 22:46:50 -0400

    Changed CallBot's name

--- bots/CallBot.c
+++ bots/CallBot.c
@@ -2,16 +2,16 @@
 
 #include "bcb-client.h"
 
-const char* BOT_NAME = "call-bot";
+const char* BOT_NAME = "CallBot";
 
 void game_setup(const struct player_data* players, unsigned int numplayers)
 {
-	
+
 }
 
 void round_start(unsigned int rnum, unsigned int pstart, unsigned int ante)
 {
-	
+
 }
 
 int player_turn(const struct player_data* players, unsigned int numplayers)
@@ -21,7 +21,7 @@ int player_turn(const struct player_data* players, unsigned int numplayers)
 
 void round_end(const struct player_data* players, unsigned int numplayers, unsigned int winnings)
 {
-	
+
 }
 
 void game_end()
