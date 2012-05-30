bb46888df668842cce9a8f39a3c81ef637989f94
Chad Waters <authorblues@gmail.com>
2012-05-30 17:51:04 -0400

    Added blank opengl visualiser

---
+++ base/bcb-opengl.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+#include "bcb-base.h"
+#include "bcb-visual.h"
+
+int setup_bcb_vis(int numagents, struct agent_t *agents, int *argc, char ***argv)
+{
+	return 1;
+};
+
+int update_bcb_vis(int numagents, struct agent_t *agents, card_t* cards, const int turn, const char *move)
+{
+
+};
+
+void close_bcb_vis()
+{
+
+};
+
